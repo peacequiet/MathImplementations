b0718b72212a628fe4d4071d6f56d0c55e442325
peacequiet <brad0912@gmail.com>
2024-07-19 19:53:50 -0700

    updated todos on node

--- Computer Algebra System/Node.py
+++ Computer Algebra System/Node.py
@@ -1,3 +1,8 @@
+# TODO: algorithm to turn postfix expression into expression tree 
+# the rough schematic will involve popping each pair of terms
+# into left and right trees and combining them as we come across
+# operations.
+
 class Node:
     def __init__(self, coefficient, power):
         self.coefficient = coefficient
